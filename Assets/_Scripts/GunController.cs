2ddc5f580e4928da03718985d9877ade2daf677d
KkRrtT <wolfstein546@gmail.com>
2023-08-03 13:43:01 +0200

    Ammo implemented

--- Assets/_Scripts/GunController.cs
+++ Assets/_Scripts/GunController.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using OpenCover.Framework.Model;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -15,18 +16,23 @@ public class GunController : MonoBehaviour
     public float damage;
     GameManager manager;
     private int m14BulletsFull = 30;
-    [SerializeField] int m14Bullets;
+    [SerializeField] int m14Bullets = default;
     private int handgunBulletsFull = 10;
     private int handgunBullets = default;
     public float aimBloom = 10;
     [SerializeField] float speed = 50;
 
-    bool canShoot = true;
+    [SerializeField] bool canShootPistol = true;
+    [SerializeField] bool canShootGun = true;
 
     private void Start()
     {
         manager = FindObjectOfType<GameManager>();
         aimBloom = 45;
+        handgunBulletsFull = 10;
+        handgunBullets = 10;
+        m14BulletsFull = 30;
+        m14Bullets = 30;
     }
 
     private void Update()
@@ -34,42 +40,41 @@ public class GunController : MonoBehaviour
         if (manager.mode == false)
         {
             //aimBloom = 20;
-            if (canShoot && Input.GetMouseButtonDown(0))
+            if (canShootPistol && Input.GetMouseButtonDown(0))
             {
                 StartCoroutine(MuzzleFlash(muzzleFlashPistol));
-                ShootBullet();
+                ShootBullet(ref handgunBullets, ref canShootPistol);
             }
             if (Input.GetKeyDown(KeyCode.R))
             {
-                Reload(manager.mode);
+                StartCoroutine(ReloadPistol());
             }
         }
         else if (manager.mode == true)
         {
             //aimBloom = 45;
-            if (canShoot && Input.GetMouseButton(0))
+            if (canShootGun && Input.GetMouseButton(0))
             {
                 fireRate -= Time.deltaTime;
                 if (fireRate <= 0)
                 {
                     fireRate = fireRateGun;
                     StartCoroutine(MuzzleFlash(muzzleFlashGun));
-                    ShootBullet();
-
+                    ShootBullet(ref m14Bullets, ref canShootGun);
                 }
-
-                //StartCoroutine(FireRate());
             }
-            if (canShoot && Input.GetMouseButtonDown(0))
+            if (canShootGun && Input.GetMouseButtonDown(0))
             {
                 StartCoroutine(MuzzleFlash(muzzleFlashGun));
-                ShootBullet();
-
-
+                ShootBullet(ref m14Bullets, ref canShootGun);
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                StartCoroutine(ReloadGun());
             }
         }
     }
-    private void ShootBullet()
+    private void ShootBullet(ref int usedGunAmmo, ref bool canShoot)
     {
         float x = Screen.width / 2;
         float y = Screen.height / 2;
@@ -77,18 +82,22 @@ public class GunController : MonoBehaviour
         float xAcc = Random.Range(x - aimBloom, x + aimBloom);
         float yAcc = Random.Range(y - aimBloom, y + aimBloom);
 
-        print(yAcc);
-        print(xAcc);
+        //print(yAcc);
+        //print(xAcc);
 
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(xAcc, yAcc, 0));
 
 
         GameObject bullet = Instantiate(bulletPrefab, muzzlePoint.position, muzzlePoint.rotation);
+        usedGunAmmo--;
         Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
         Bullet bulletScript = bullet.GetComponent<Bullet>();
         bulletRB.velocity = speed * ray.direction;
 
-
+        if(usedGunAmmo <= 0)
+        {
+            canShoot = false;
+        }
 
         if (bulletScript != null)
         {
@@ -104,36 +113,35 @@ public class GunController : MonoBehaviour
             enemy.TakeDamageEnemy(damage);
         }
     }
-    private void Reload(bool mode)
-    {
-        float timer = 1.5f;
-        timer -= Time.deltaTime;
-        if (timer < 0 && mode == false)
-        {
-            handgunBullets = handgunBulletsFull;
-            canShoot = true;
-        }
-        else if (timer < 0 && mode == true)
-        {
-            m14Bullets = m14BulletsFull;
-            canShoot = true;
-        }
-    }
 
-    private void Flash(GameObject flash)
-    {
-        flash.SetActive(true);
-    }
+
 
     private IEnumerator MuzzleFlash(GameObject flash)
     {
         flash.SetActive(true);
-        print("Routine started");
+        //print("Routine started");
 
         yield return new WaitForSeconds(0.05f);
 
         flash.SetActive(false);
-        print("Routine finished");
+        //print("Routine finished");
+    }
+
+    private void ReloadM(ref int a, int b, ref bool c)
+    {
+        a = b;
+        c = true;
+        print("Metoda");
+    }
+    private IEnumerator ReloadGun()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ReloadM( ref m14Bullets, m14BulletsFull, ref canShootGun); 
+    }
+    private IEnumerator ReloadPistol()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ReloadM(ref handgunBullets, handgunBulletsFull, ref canShootPistol);
     }
 
 }
